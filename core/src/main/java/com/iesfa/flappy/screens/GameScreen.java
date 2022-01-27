21185258236e331b47e61af0c53362cfacca6850
JC <jcalfaroprof@gmail.com>
2022-01-27 19:58:53 +0100

    Cambios menores

--- core/src/main/java/com/iesfa/flappy/screens/GameScreen.java
+++ core/src/main/java/com/iesfa/flappy/screens/GameScreen.java
@@ -1,21 +1,24 @@
 package com.iesfa.flappy.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.iesfa.flappy.MainGame;
 
 import sun.tools.jar.Main;
 
 
-//Todo: Todas la pantallas deberán implementar la interfaz Screen ya que es la que controla el ciclo de vida
-// de esta. ¿Habría una forma mejor de organizar el código?
 public class GameScreen extends BaseScreen {
 
 
-    //Todo : Toda pantalla debe tener una 'escena' que controle los elementos que aparecen en cada
+    //Todo alumno: Toda pantalla debe tener una 'escena' que controle los elementos que aparecen en cada
     // pantalla (Podríamos decir que sería nuestro director de escena)
+
+
 
     public GameScreen(MainGame mainGame){
         super(mainGame);
+
     }
 
     @Override
