a25ae346376d78b7da64104daa6e90919f038edd
JC <jcalfaroprof@gmail.com>
2022-01-27 14:02:31 +0100

    Proyecto inicial

---
+++ android/src/main/java/com/iesfa/flappy/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.iesfa.flappy.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.iesfa.flappy.flappyBirdMain;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new flappyBirdMain(), configuration);
+	}
+}
