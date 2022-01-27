df559aedb2dffa57a4687fcc72a90d4c15ff2697
JC <jcalfaroprof@gmail.com>
2022-01-27 17:58:02 +0100

    Añadidos comentarios

--- core/src/main/java/com/iesfa/flappy/flappyBirdMain.java
+++ core/src/main/java/com/iesfa/flappy/flappyBirdMain.java
@@ -8,7 +8,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class flappyBirdMain extends ApplicationAdapter {
+
+	//Objeto encargado de 'comunicarse' con la tarjeta gráfica
 	private SpriteBatch batch;
+
+	//Objeto encargado de cargar la imagen de nuestros archvios a la memoria de la tarjeta gráfica.
 	private Texture image;
 
 	@Override
@@ -19,17 +23,23 @@ public class flappyBirdMain extends ApplicationAdapter {
 
 	@Override
 	public void render() {
+
+		//Estas lineas son necesarias para limpiar la pantalla y lo contenido en el buffer gráfico
+		// de la iteración anterior
 		Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		///Cuando vayamos a decirle a la gráfica lo que queramos dibujar debemos hacerlo por lotes.
 		batch.begin();
 		batch.draw(image, 140, 210);
 		batch.end();
-
-		System.out.println("Hola mundo");
 	}
 
 	@Override
 	public void dispose() {
+
+		//Recordamos que tanto la imagen como el batch están cargados en la gráfica. Por lo que
+		// cuando salgamos de la pantalla debemos liberar los recursos
 		batch.dispose();
 		image.dispose();
 	}
